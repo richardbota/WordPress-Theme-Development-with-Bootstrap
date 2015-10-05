c8fe11e8999250e037d378093b5bb679a3cf931a
Richard Bota <richard.bota@khk.ee>
2015-10-05 12:19:20 +0300

    Lecture 15 - Install Free Icon Fonts with FontAwesome

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by admin on 10/4/2015.
+ */
---
+++ index.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html";
+          charset="UTF-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/img/favicon.ico">
+    <title>Bootstrap to WordPress</title>
+
+    <!-- Font Awesome Icons -->
+    <link href="assets/css/font-awesome/css/font-awesome.min.css"
+        rel="stylesheet">
+
+    <!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <!-- Header -->
+    <header>
+
+    </header>
+
+
+    <!-- Hero -->
+    <section>
+
+    </section>
+
+
+    <!-- Opt in section -->
+    <section>
+
+    </section>
+
+
+    <!-- Boost your income -->
+    <section>
+
+    </section>
+
+
+    <!-- Who benefits -->
+    <section>
+
+    </section>
+
+
+    <!-- Course Features -->
+    <section>
+
+    </section>
+
+
+    <!-- Project Features -->
+    <section>
+
+    </section>
+
+
+    <!-- Video featurette -->
+    <section>
+
+    </section>
+
+
+    <!-- Instructor -->
+    <section>
+
+    </section>
+
+
+    <!-- Testimonials -->
+    <section>
+
+    </section>
+
+
+    <!-- Sign up section -->
+    <section>
+
+    </section>
+
+
+    <!-- Footer -->
+    <footer>
+
+    </footer>
+
+
+    <!-- Modal -->
+    <div>
+
+    </div>
+
+    <!-- Bootstrap core javascript
+    Placed at the end of the document so the pages load faster
+    -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="assets/js/jquery-2.1.4.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+</html>
