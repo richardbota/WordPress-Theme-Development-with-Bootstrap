266821c85c329c9f03f3db927cc9c5427bfbd37a
Richard Bota <richard.bota@khk.ee>
2015-10-11 20:26:03 +0300

    Lecture 22: Learn How to Create a Modal Popup using Bootstrap [Delivers #105401926]

--- index.php
+++ index.php
@@ -180,15 +180,48 @@
 
 
     <!-- Modal -->
-    <div>
+    <div class="modal fade" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
 
-    </div>
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i> Subscribe to our Mailing List</h4>
+                </div><!-- modal-header -->
+
+                <div class="modal-body">
+                    <p>Simply enter your name and email! As a thank you for joining us, we're going to give you one of our best-selling courses, <em>for free!</em></p>
+
+                    <form class="form-inline" role="form">
+                        <div class="form-group">
+                            <label class="sr-only" for="subscribe-name">Your first name</label>
+                            <input type="text" class="form-control" id="subscribe-name" placeholder="Your first name">
+                        </div>
+                        <div class="form-group">
+                            <label class="sr-only" for="subscribe-email">and your email</label>
+                            <input type="text" class="form-control" id="subscribe-email" placeholder="and your email">
+                        </div>
+                        <input type="submit" class="btn btn-danger" value="Subscribe!">
+                    </form>
+
+                    <hr>
+
+                    <p><small>By providing your email you consent to receiving occasional promotional emails &amp; newsletters. <br>No Spam. Just good stuff. We respect your privacy &amp; you may unsubscribe at any time.</small></p>
+                </div><!-- modal-body -->
+
+            </div><!-- modal-content -->
+        </div><!-- modal-dialog -->
+    </div><!-- modal -->
 
     <!-- Bootstrap core javascript
     Placed at the end of the document so the pages load faster
     -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
     <script src="assets/js/jquery-2.1.4.min.js"></script>
+
+    <script src="assets/js/bootstrap.min.js"></script>
+
     <script src="assets/js/main.js"></script>
 
     <!-- Adobe Typekit Fonts -->
