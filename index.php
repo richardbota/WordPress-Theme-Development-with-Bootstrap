6fb05bee31ee2542558ddfdaf3a6b6b350b326b9
Richard Bota <richard.bota@khk.ee>
2015-10-15 14:15:09 +0300

    Lecture 32: Code the Sign Up Section [Delivers #105692972]

--- index.php
+++ index.php
@@ -409,9 +409,16 @@
 
 
     <!-- Sign up section -->
-    <section>
-
-    </section>
+    <section id="signup" data-type="background" data-speed="4">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <h2>Are you ready to take your coding skills to the <strong>next level</strong>?</h2>
+                    <p><a href="" class="btn btn-lg btn-block btn-success">Yes, sign me up!</a></p>
+                </div><!-- end col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </section><!-- signup -->
 
 
     <!-- Footer -->
