68a85e32569a09fcbde158a75146e5c2e4b396c5
Richard Bota <richard.bota@khk.ee>
2015-10-22 23:00:00 +0300

    Lecture 49: Cover Your Assets (Adding Your Styles, Scripts, Fonts & Images) [Delivers #105932320]

--- wp-content/themes/bootstrap-to-wordpress/header.php
+++ wp-content/themes/bootstrap-to-wordpress/header.php
@@ -17,7 +17,23 @@
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
+    <!-- Bootstrap core CSS -->
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome Icons -->
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css"
+          rel="stylesheet">
+
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+
     <?php wp_head(); ?>
+
+    <!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
