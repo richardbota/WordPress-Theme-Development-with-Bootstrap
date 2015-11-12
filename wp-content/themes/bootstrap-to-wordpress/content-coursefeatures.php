3f58b55bdee8ebda6ab0b699daaf0993579d09b8
Richard Bota <richard.bota@khk.ee>
2015-11-12 22:42:04 +0200

    Lecture 63: Create a Custom Resources Template [Delivers #105932348]

---
+++ wp-content/themes/bootstrap-to-wordpress/content-coursefeatures.php
@@ -0,0 +1,52 @@
+<?php
+
+$feature_section_image  =get_field('feature_section_image');
+$feature_section_title  =get_field('feature_section_title');
+$feature_section_body   =get_field('feature_section_body');
+
+?>
+
+<!-- Course Features -->
+<section id="course-features">
+    <div class="container">
+
+        <div class="section-header">
+
+            <!-- If user uploaded an image -->
+            <?php if (!empty($feature_section_image)) : ?>
+
+                <img src="<?php echo $feature_section_image['url']; ?>"
+                     alt="<?php echo $feature_section_image['alt']; ?>">
+
+            <?php endif; ?>
+
+            <h2><?php echo $feature_section_title; ?></h2>
+
+            <!-- If user added body text -->
+            <?php if (!empty($feature_section_body)) : ?>
+
+                <p class="lead"><?php echo $feature_section_body; ?></p>
+
+            <?php endif; ?>
+        </div>
+        <!-- section-header -->
+
+        <div class="row">
+
+            <?php $loop = new WP_Query(array('post_type' => 'course_feature',
+                'orderby' => 'post_id', 'order' => 'ASC')); ?>
+
+            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+
+                <div class="col-sm-2">
+                    <i class="<?php the_field('course_feature_icon'); ?>"></i>
+                    <h4><?php the_title(); ?></h4>
+                </div><!-- end col -->
+
+            <?php endwhile; wp_reset_query(); ?>
+
+        </div>
+        <!-- row -->
+    </div>
+    <!-- container -->
+</section><!-- course-features -->
