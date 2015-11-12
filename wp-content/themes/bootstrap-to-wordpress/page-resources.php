4531d4fdc73fc9922d16778fa9664d1a95d0b0fe
Richard Bota <richard.bota@khk.ee>
2015-11-12 23:11:55 +0200

    Lecture 64: Add a Feature Image, and a Fallback Image [Delivers #105932350]

--- wp-content/themes/bootstrap-to-wordpress/page-resources.php
+++ wp-content/themes/bootstrap-to-wordpress/page-resources.php
@@ -1,13 +1,27 @@
 <?php
-/*
-    Template Name: Resources Page
-*/
+/* Template Name: Resources Page */
+
+get_header();
+
+$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
 ?>
 
 <!-- Feature Image -->
+<?php if( has_post_thumbnail() ){ // check for feature image ?>
+
+    <section class="feature-image"
+             style="background: url('<?php echo $thumbnail_url; ?>') no-repeat; background-size: cover;"
+             data-type="background" data-speed="2">
+        <h1 class="page-title"><?php the_title(); ?></h1>
+    </section>
+
+<?php } else { // Fallback image ?>
+
 <section class="feature-image feature-image-default" data-type="background" data-speed="2">
-    <h1 class="page-title">Resources</h1>
+    <h1 class="page-title"><?php the_title(); ?></h1>
 </section><!-- Feature Image end -->
+
+<?php } ?>
 
 <!-- Main Content -->
 <div class="container">
@@ -86,4 +100,6 @@
     </div>
     <!-- primary -->
 </div><!-- container -->
+
+<?php get_footer(); ?>
 
