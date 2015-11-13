930e24689806be071dd13495f42619f07eb24c4f
Richard Bota <richard.bota@khk.ee>
2015-11-13 19:06:51 +0200

    Lecture 66: Easily Set Up Your Custom Blog Index Page [Delivers #105932354]

--- wp-content/themes/bootstrap-to-wordpress/index.php
+++ wp-content/themes/bootstrap-to-wordpress/index.php
@@ -14,43 +14,57 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+    <h1 class="page-title">Blog</h1>
+</section>
 
-        <?php if (have_posts()) : ?>
+<!-- Blog Content -->
+<div class="container">
+    <div class="row" id="primary">
 
-            <?php if (is_home() && !is_front_page()) : ?>
-                <header>
-                    <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-                </header>
+        <main id="content" class="col-sm-8" role="main">
+
+            <?php if (have_posts()) : ?>
+
+                <?php if (is_home() && !is_front_page()) : ?>
+                    <header>
+                        <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                    </header>
+                <?php endif; ?>
+
+                <?php /* Start the Loop */ ?>
+                <?php while (have_posts()) : the_post(); ?>
+
+                    <?php
+
+                    /*
+                     * Include the Post-Format-specific template for the content.
+                     * If you want to override this in a child theme, then include a file
+                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                     */
+                    get_template_part('template-parts/content', get_post_format());
+                    ?>
+
+                <?php endwhile; ?>
+
+                <?php the_posts_navigation(); ?>
+
+            <?php else : ?>
+
+                <?php get_template_part('template-parts/content', 'none'); ?>
+
             <?php endif; ?>
 
-            <?php /* Start the Loop */ ?>
-            <?php while (have_posts()) : the_post(); ?>
 
-                <?php
+        </main><!-- Content -->
 
-                /*
-                 * Include the Post-Format-specific template for the content.
-                 * If you want to override this in a child theme, then include a file
-                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                 */
-                get_template_part('template-parts/content', get_post_format());
-                ?>
+        <!-- SIDEBAR
+			================================================== -->
+        <aside class="col-sm-4">
+            <?php get_sidebar(); ?>
+        </aside>
 
-            <?php endwhile; ?>
+    </div><!-- Primary -->
+</div><!-- Container -->
 
-            <?php the_posts_navigation(); ?>
-
-        <?php else : ?>
-
-            <?php get_template_part('template-parts/content', 'none'); ?>
-
-        <?php endif; ?>
-
-    </main>
-    <!-- #main -->
-</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
