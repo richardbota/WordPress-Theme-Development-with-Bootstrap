6d105681041f53c2198ddb3c5e213f489f9a6067
Richard Bota <richard.bota@khk.ee>
2015-11-14 14:40:09 +0200

    Lecture 69: Style and Customize the Comments Section [Delivers #105932360]

--- wp-content/themes/bootstrap-to-wordpress/comments.php
+++ wp-content/themes/bootstrap-to-wordpress/comments.php
@@ -22,18 +22,18 @@ if (post_password_required()) {
 
 <div id="comments" class="comments-area">
 
-    <?php // You can start editing here -- including this comment! ?>
+    <div class="comments-wrap">
 
     <?php if (have_comments()) : ?>
-        <h2 class="comments-title">
+        <h3 class="comments-title">
             <?php
             printf( // WPCS: XSS OK.
-                esc_html(_nx('One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'bootstrap-to-wordpress')),
+                esc_html(_nx('One comment on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'bootstrap-to-wordpress')),
                 number_format_i18n(get_comments_number()),
                 '<span>' . get_the_title() . '</span>'
             );
             ?>
-        </h2>
+        </h3>
 
         <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : // Are there comments to navigate through? ?>
             <nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
@@ -87,4 +87,5 @@ if (post_password_required()) {
 
     <?php comment_form(); ?>
 
+    </div><!-- Comments -->
 </div><!-- #comments -->
