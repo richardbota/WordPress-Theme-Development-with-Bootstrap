fe71eef660fa23b9979e0289eb867791de1f26af
Richard Bota <richard.bota@khk.ee>
2015-11-14 23:12:46 +0200

    Lecture 74: Adding & Customizing Your Sidebar Widgets [Delivers #105932370]

--- wp-content/themes/bootstrap-to-wordpress/functions.php
+++ wp-content/themes/bootstrap-to-wordpress/functions.php
@@ -111,6 +111,16 @@ function bootstrap_to_wordpress_widgets_init()
         'before_title' => '<h4 class="widget-title">',
         'after_title' => '</h4>',
     ));
+
+    register_sidebar(array(
+        'name' => esc_html__('Extra', 'bootstrap-to-wordpress'),
+        'id' => 'sidebar-2',
+        'description' => '',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h4 class="widget-title">',
+        'after_title' => '</h4>',
+    ));
 }
 
 add_action('widgets_init', 'bootstrap_to_wordpress_widgets_init');
