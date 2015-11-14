27ff32e0b5a2ef7f9f601030b65b76b806522358
Richard Bota <richard.bota@khk.ee>
2015-11-14 23:48:18 +0200

    Lecture 75: Register and Organize Your Custom WordPress Menus [Delivers #105932372]

--- wp-content/themes/bootstrap-to-wordpress/functions.php
+++ wp-content/themes/bootstrap-to-wordpress/functions.php
@@ -46,6 +46,7 @@ if (!function_exists('bootstrap_to_wordpress_setup')) :
         // This theme uses wp_nav_menu() in one location.
         register_nav_menus(array(
             'primary' => esc_html__('Primary Menu', 'bootstrap-to-wordpress'),
+            'footer' => esc_html__('Footer Menu', 'bootstrap-to-wordpress'),
         ));
 
         /*
