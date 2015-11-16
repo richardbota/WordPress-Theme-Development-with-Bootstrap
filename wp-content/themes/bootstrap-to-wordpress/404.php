fbfcb6caa589514747124f6a0b0a68bde41880f8
Richard Bota <richard.bota@khk.ee>
2015-11-16 14:26:05 +0200

    Lecture 76: Create a Useful & Engaging 404 Page [Delivers #105932374]

--- wp-content/themes/bootstrap-to-wordpress/404.php
+++ wp-content/themes/bootstrap-to-wordpress/404.php
@@ -2,61 +2,105 @@
 /**
  * The template for displaying 404 pages (not found).
  *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package Bootstrap_to_Wordpress
+ * @package Bootstrap to WordPress
  */
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+    <h1 class="page-title">Bummer! That page can't be found.</h1>
+</section>
 
-        <section class="error-404 not-found">
-            <header class="page-header">
-                <h1 class="page-title"><?php esc_html_e('Oops! That page can&rsquo;t be found.', 'bootstrap-to-wordpress'); ?></h1>
-            </header>
-            <!-- .page-header -->
+<div class="container">
 
-            <div class="page-content">
-                <p><?php esc_html_e('It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bootstrap-to-wordpress'); ?></p>
+    <div id="primary" class="row">
 
-                <?php get_search_form(); ?>
+        <main id="content" class="col-sm-8">
 
-                <?php the_widget('WP_Widget_Recent_Posts'); ?>
+            <div class="error-404 not-found">
 
-                <?php if (bootstrap_to_wordpress_categorized_blog()) : // Only show the widget if site has multiple categories. ?>
+                <div class="page-content">
+
+                    <h2>Don't fret! Let's get you back on track.</h2>
+
+                    <!-- RESOURCES
+                    ================================================== -->
+                    <h3>Resources</h3>
+                    <p>Perhaps you were looking for a specific resource?</p>
+
+                    <?php $loop = new WP_Query( array( 'post_type' => 'resource', 'orderby'=>'post_id', 'order'=>'ASC' ) ); ?>
+
+                    <div class="resource-row clearfix">
+
+                        <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+
+                            <?php
+                            $resource_image	= get_field('resource_image');
+                            $resource_url	= get_field('resource_url');
+                            $button_text	= get_field('button_text');
+                            ?>
+
+                            <div class="resource">
+
+                                <img src="<?php echo $resource_image[url]; ?>" alt="<?php echo $resource_image[alt]; ?>">
+
+
+                                <h3><a href="<?php echo $resource_url; ?>"><?php the_title(); ?></a></h3>
+                                <?php the_excerpt(); ?>
+
+                                <?php if( !empty($button_text) ) : ?>
+                                    <a href="<?php echo $resource_url; ?>" class="btn btn-success"><?php echo $button_text; ?></a>
+                                <?php endif; ?>
+                            </div><!-- resource -->
+
+                        <?php endwhile; ?>
+
+                    </div><!-- resource-row -->
+
+                    <!-- CATEGORIES
+                    ================================================== -->
+                    <h3>Categories</h3>
+                    <p>...or maybe a popular category?</p>
+
                     <div class="widget widget_categories">
-                        <h2 class="widget-title"><?php esc_html_e('Most Used Categories', 'bootstrap-to-wordpress'); ?></h2>
+                        <h4 class="widget-title">Most Used Categories</h4>
                         <ul>
                             <?php
-                            wp_list_categories(array(
-                                'orderby' => 'count',
-                                'order' => 'DESC',
-                                'show_count' => 1,
-                                'title_li' => '',
-                                'number' => 10,
-                            ));
+                            wp_list_categories( array (
+
+                                'orderby'	=> 'count',
+                                'order'		=> 'DESC',
+                                'show_count'=> 1,
+                                'title_li'	=> '',
+                                'number'	=> 10
+
+                            ) );
                             ?>
                         </ul>
                     </div><!-- .widget -->
-                <?php endif; ?>
 
-                <?php
-                /* translators: %1$s: smiley */
-                $archive_content = '<p>' . sprintf(esc_html__('Try looking in the monthly archives. %1$s', 'bootstrap-to-wordpress'), convert_smilies(':)')) . '</p>';
-                the_widget('WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content");
-                ?>
+                    <!-- ARCHIVES
+                    ================================================== -->
+                    <h3>Archives</h3>
+                    <p>You can always sort through our archives...</p>
+                    <?php the_widget( 'WP_Widget_Archives', 'title=Our Archives', 'before_title=<h4 class="widgettitle">&after_title=</h4>' ); ?>
 
-                <?php the_widget('WP_Widget_Tag_Cloud'); ?>
+                    <p>...or, just head back to the <a href="<?php echo esc_url( home_url( '/' ) ); ?>">home page</a>.</p>
 
-            </div>
-            <!-- .page-content -->
-        </section>
-        <!-- .error-404 -->
+                </div><!-- .page-content -->
 
-    </main>
-    <!-- #main -->
-</div><!-- #primary -->
+            </div><!-- .error-404 -->
+
+        </main><!-- #content -->
+
+        <!-- SIDEBAR
+        ================================================== -->
+        <aside class="col-sm-4">
+            <?php get_sidebar(); ?>
+        </aside>
+
+    </div><!-- #primary -->
+
+</div><!-- .container -->
 
 <?php get_footer(); ?>
